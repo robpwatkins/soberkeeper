2251e84d58d16859f0ac3ac4c0c8c5f9dda07fac
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-29 22:46:48 -0500

    hmmm

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  }
+
+  const handleClick = () => {
+    callBackendAPI()
+      .then(res => console.log(res));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +34,7 @@ function App() {
         >
           Learn React
         </a>
+      <button onClick={handleClick}>Click ME!</button>
       </header>
     </div>
   );
