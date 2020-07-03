3c898667d0efa1cfc19e2c033c6cb223d8417506
Rob W <robpwatkins@gmail.com>
2020-07-03 16:33:17 -0500

    INSERTing into test postssssss

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Thefatlady3!',
   database: 'testerDB'
 });
 
-connection.connect(err => {
+con.connect(err => {
   if (err) throw err;
-  console.log('You did it!');
+  console.log('You did it!!!');
+});
+
+con.query('SELECT * FROM testPosts', (err, rows) => {
+  if (err) throw err;
+  console.log('Got your DB data here:');
+  console.log(rows);
 })
+
+// const user = { ID: 4, username: 'testUser4', post: 'Can you believe it?' };
+// con.query('INSERT INTO testPosts SET ?', user, (err, res) => {
+//   if (err) throw err;
+//   console.log('Last insert ID:', res.insertId);
+// })
+
+con.end(err => {
+
+});
