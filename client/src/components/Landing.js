d70cceb8786c6cc29348cfd523c309059c7d8b87
Rob W <robpwatkins@gmail.com>
2020-07-04 19:18:33 -0500

    close to a stopping point

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Badge from './Badge';
+import Outward from './Outward';
 
 const Landing = () => {
   return (
     <div className="landing">
       <div className="post-area">
         <Badge />
+        <div className="outward-container">
+          <Outward />
+          <Outward />
+          <Outward />
+          <Outward />
+          <Outward />
+          <Outward />
+          <Outward />
+          <Outward />
+        </div>
       </div>
     </div>
   )
