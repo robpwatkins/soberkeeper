fd9715eb2b851f7cb3604635d30a529480747c50
Rob W <robpwatkins@gmail.com>
2020-07-05 11:48:09 -0500

    slowly and surely

--- client/src/components/Badge.js
+++ client/src/components/Badge.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Badge = () => {
   return (
     <div className="badge-container">
-      <div className="badge"></div>
+      <div className="badge">
+        <div className="title"></div>
+      </div>
     </div>
   )
 }
