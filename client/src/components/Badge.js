ed914300975d8648daf9d904cb5353617b7eca62
Rob W <robpwatkins@gmail.com>
2020-07-08 09:47:37 -0500

    nah

--- client/src/components/Badge.js
+++ client/src/components/Badge.js
@@ -11,7 +11,4 @@ const Badge = () => {
   )
 }
 
-https://courses.zollege.com/courses/course-v1:ACA+JS411+09282020_JS411_C6/courseware/22c4b12233c349659373e96b5c5e26f4/f204f7739b1e459690615a8af65ff9dc/2?activate_block_id=block-v1%3AACA%2BJS411%2B09282020_JS411_C6%2Btype%40vertical%2Bblock%40bee86892ddcd446d94d9d13d8b1c273a
-https://courses.zollege.com/courses/course-v1:ACA+JS411+09282020_JS411_C6/courseware/22c4b12233c349659373e96b5c5e26f4/f204f7739b1e459690615a8af65ff9dc/2?activate_block_id=block-v1%3AACA%2BJS411%2B09282020_JS411_C6%2Btype%40vertical%2Bblock%40bee86892ddcd446d94d9d13d8b1c273a
-
 export default Badge;
