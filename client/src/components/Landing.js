32067af71da7ae24b5a1d26e6f67b93eb9967c5a
Rob W <robpwatkins@gmail.com>
2020-07-17 20:02:55 -0500

    omgggg I just uploaded a file, pushed it to cloudinary AND displayed that shiz back in puppyAPIIIII

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,6 +3,7 @@ import Outward from './Outward';
 
 const Landing = () => {
   const [posts, setPosts] = useState([]);
+  const [image, setImage] = useState(null);
 
   const getBackendAPI = async () => {
     const response = await fetch('/testPosts/outward');
@@ -19,6 +20,21 @@ const Landing = () => {
     .then(res => setPosts(res));
   }, [])
 
+  const uploadFile = async event => {
+    console.log('uploading file . . .');
+    const files = event.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'puppyAPI');
+
+    const response = await fetch('https://api.cloudinary.com/v1_1/robpwatkins/image/upload', {
+      method: 'Post',
+      body: data
+    });
+    const file = await response.json();
+    console.log(file);
+    setImage(file.secure_url);
+  }
 
   const handleClick = () => {
     getBackendAPI()
@@ -29,9 +45,57 @@ const Landing = () => {
     <div className="landing">
       <div className="post-area">
         <div className="outward-container">
-          {posts.length > 0 &&
-            <img src={posts[15].outward} alt=""/>
-          }
+        <fieldset /* disabled={loading} aria-busy={loading} */>
+              <label htmlFor="file">
+                Image
+                <input 
+                  type="file" 
+                  id="file" 
+                  name="file" 
+                  placeholder="Upload an image" 
+                  required 
+                  // value={this.state.image}
+                  onChange={uploadFile}
+                  />
+                  {image && <img width="200" src={image} alt="Upload Preview" />}
+              </label>
+              {/* <label htmlFor="title">
+                Title
+                <input 
+                  type="text" 
+                  id="title" 
+                  name="title" 
+                  placeholder="Title" 
+                  required 
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                  />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input 
+                  type="number" 
+                  id="price" 
+                  name="price" 
+                  placeholder="Price" 
+                  required 
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                  />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea 
+                  id="description" 
+                  name="description" 
+                  placeholder="Enter a description" 
+                  required 
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                  />
+              </label> */}
+              <button type="submit">SUBMIT</button>
+            </fieldset>
           {/* <Outward />
           <Outward />
           <Outward />
