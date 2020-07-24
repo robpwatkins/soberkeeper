46cd33d1e91856171526dccab0289641228d34c2
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-07-23 19:42:06 -0500

    yes

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 const App = () => {
 
-  const getBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
+  // const getBackendAPI = async () => {
+  //   const response = await fetch('/express_backend');
+  //   const body = await response.json();
 
-    if (response.status !== 200) {
-      throw Error(body.message);
-    }
-    return body;
-  }
+  //   if (response.status !== 200) {
+  //     throw Error(body.message);
+  //   }
+  //   return body;
+  // }
 
   const postBackendAPI = async () => {
     const response = await fetch('/express_backend', {
